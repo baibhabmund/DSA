0633215c2b21cd5a7f673b5bd28c7724cb53d42e
baibhabmund <baibhab.mund20@gmail.com>
2025-02-04 10:36:26 +0530

    Sorting an array

---
+++ src/Sorting.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+public class Sorting {
+    public static void main(String[] args) {
+        int[] arr = {43,2,56,32,12,65};
+        Arrays.sort(arr);
+        System.out.println("Sorted Array : " + Arrays.toString(arr));
+    }
+}
