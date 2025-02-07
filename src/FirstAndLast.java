d7b2bf9b5e09417e88eec9197635bf89e62a24ad
baibhabmund <baibhab.mund20@gmail.com>
2025-02-07 12:35:14 +0530

    Find the first and last element in the Array List

---
+++ src/FirstAndLast.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class FirstAndLast {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        for(int i = 0; i < list.size(); i++){
+            System.out.print(list.get(i) + " ");
+        }
+        System.out.println();
+        System.out.println("First Element = " + list.getFirst() + " Last Element = " + list.getLast());
+    }
+}
